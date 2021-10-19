41b222dfb4705a7615345039d59b8e43ac4973e9
PedroUnreal <pedrounreal@gmail.com>
2021-10-19 21:47:04 +0300

    Fixed broken page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -49,7 +49,7 @@ const getPictureInfo = function (id = 0) {
 
 /**
  * Функция показывает индикатор загрузки.
- * Меняет ситили, ничего не возвращает.
+ * Меняет стили, ничего не возвращает.
  */
 const showLoader = function () {
     loader.style.visibility = 'visible';
@@ -62,7 +62,7 @@ const showLoader = function () {
 const hideLoader = function () {
     loaderTimeout = setTimeout(function () {
         loader.style.visibility = 'hidden';
-        loaderTimeout.clearTimeout();
+        clearTimeout(loaderTimeout);
     }, 700);
 }
 
@@ -91,10 +91,11 @@ const renderPictures = function (list) {
         throw Error(`Pictures not defined. The list length: ${list.length}`);
     }
 
-    const clone = templateImageCard.content.cloneNode(true);
     const fragment = document.createDocumentFragment();
 
     list.forEach(function (element) {
+        const clone = templateImageCard.content.cloneNode(true);
+
         const link = clone.querySelector('a');
 
         link.href = element.url;
@@ -135,7 +136,7 @@ const renderPopupPicture = function (picture) {
 }
 
 /**
- * Функция переклбчает класс открытия на попапе
+ * Функция переключает класс открытия на попапе
  */
 const togglePopup = function () {
     popup.classList.toggle('open');
@@ -171,8 +172,10 @@ const actionHandler = function (evt) {
 const imageHandler = function (evt) {
     evt.preventDefault();
 
-    if (evt.target.closest('a')) {
-        getPictureInfo(evt.target.dataset.id);
+    const dataId = evt.target.parentNode.getAttribute('data-id');
+
+    if (dataId) {
+        getPictureInfo(dataId);
     }
 }
 
